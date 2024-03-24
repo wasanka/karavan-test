0fcfc9075daade0469e70717a8d951828943a17c
WASANKA WICKRAMASINGHE <wasanka2000@gmail.com>
2024-03-24 03:46:03 +0000

    test

---
+++ Test/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Hi");
+        frame.setSize(600, 800);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        System.out.println("Hello world!");
+    }
+}
