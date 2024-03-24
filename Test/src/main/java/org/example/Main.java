d3b3dd1c22e6453aa0be717d1747aee5136381a6
WASANKA WICKRAMASINGHE <wasanka2000@gmail.com>
2024-03-24 03:55:17 +0000

    test

--- Test/src/main/java/org/example/Main.java
+++ Test/src/main/java/org/example/Main.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
 
-        JFrame frame = new JFrame("Hi");
-        frame.setSize(600, 800);
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+//        JFrame frame = new JFrame("Hi");
+//        frame.setSize(600, 800);
+//        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+//        frame.setVisible(true);
 
         System.out.println("Hello world!");
     }
